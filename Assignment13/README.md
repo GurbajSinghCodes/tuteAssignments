9d100ee516cf4367a6468eb6c67623517cdb66ef
Gurbaj Singh <gurbajsingh098@gmail.com>
2026-03-16 16:30:25 +0530

    Initial Commit

---
+++ Assignment13/README.md
@@ -0,0 +1,5 @@
+# To check whether a number is even or odd
+- This assignment uses simple prompts to take inputs an alerts to give message to users
+- Users need to open the [html file](index.html) 
+- Enter the number that needs to be checked
+- An alert states whether the number is even or odd
---
+++ Assignment13/script.js
@@ -0,0 +1,35 @@
+let a, b, c, d
+a = prompt("Enter the number to check as even or odd")
+if (a % 2 == 0) {
+    alert("Entered number is even")
+
+}
+else {
+    alert("Entered number is odd")
+}
+b = prompt("Enter the number to check as even or odd")
+
+if (b % 2 == 0) {
+    alert("Entered number is even")
+
+}
+else {
+    alert("Entered number is odd")
+}
+c = prompt("Enter the number to check as even or odd")
+if (c % 2 == 0) {
+    alert("Entered number is even")
+
+}
+else {
+    alert("Entered number is odd")
+}
+d = prompt("Enter the number to check as even or odd")
+
+if (d % 2 == 0) {
+    alert("Entered number is even")
+
+}
+else {
+    alert("Entered number is odd")
+}
